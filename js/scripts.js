d8e481046a6d1fd37f7c25eb1d9324910f973a1a
gustsu <justintew@eagles.ewu.edu>
2015-07-06 12:31:26 -0700

    added owl carousel

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,18 @@
 //$("h1").lettering();
 
 //$('p').lettering('words');
+
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true,
+    loop:true,
+    items: 3,
+    nav: true,
+    dots: true,
+    autoplay: true
+  
+  
+  });
+});
